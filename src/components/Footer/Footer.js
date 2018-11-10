068910aeed217eeb59c254406be306ba8822bb4e
nicho.guimbaolibot-ubidy <nicho.guimbaolibot@ubidy.com>
2018-11-11 03:35:59 +0800

    removed footer words

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Footer extends Component {
   render() {
-    return <section className="Footer-section">im a footer</section>;
+    return <section className="Footer-section"></section>;
   }
 }
 
